3befcf02adfb5326ee93897257bbfbbea945eeac
JR <josequintana2804@gmail.com>
2022-12-03 07:44:17 -0500

    Improve home page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,25 @@
+const plugin = require("tailwindcss/plugin");
+
+const customStyles = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".blizzard-shadow": {
+      filter: "drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.4));",
+      "text-shadow": "0px 1px 35px rgb(0 43 82 / 40%)",
+    },
+    ".realm-border": {
+      "border-image-source":
+        "url(https://res.cloudinary.com/jr-cloud/image/upload/v1670023140/rog-realms/rc-media-frame-spritesheet_ewjco5.png)",
+      "border-image-slice": "120",
+      "border-image-repeat": "repeat",
+      "will-change": "filter",
+    },
+    ".realm-card-divider": {
+      "background-size": "auto 60px",
+      "background-position": "bottom center, 0",
+    },
+  });
+});
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -9,5 +31,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [customStyles],
 };
